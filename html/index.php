0b4ad5836780b4b5d7223baa6c2054ac862a9e8d
Daniel Aleksandersen <code@daniel.priv.no>
2012-07-05 22:22:04 +0200

    Script type is not needed in HTML5.

--- html/index.php
+++ html/index.php
@@ -25,25 +25,20 @@ $sDevLink = "http://github.com/jeromeschneider/Baikal";
   <!--[if lt IE 9]>
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
-	<script type="text/javascript">
-		
+	<script>	
 		function goToByScroll(element) {
 			$("html,body").animate({scrollTop: $(element).offset().top}, "slow");
 		}
-		
 	</script>
-	<script type="text/javascript">
-
+	<script>
 	  var _gaq = _gaq || [];
 	  _gaq.push(['_setAccount', 'UA-31919509-1']);
 	  _gaq.push(['_trackPageview']);
-
 	  (function() {
 	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	  })();
-
 	</script>
 </head>
 <body>
