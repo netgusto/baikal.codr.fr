ee5383fe7723b30fc1f6834873d1eb260c65fa5f
Jérôme Schneider <mail@jeromeschneider.fr>
2012-08-06 10:17:35 +0200

    Minor updates on design; added the 'Fork me on github' ribbon

--- html/index.php
+++ html/index.php
@@ -273,7 +273,7 @@ $sDevLink = "http://github.com/jeromeschneider/Baikal";
 			</div>
 		</div>
 	</section>
-	
+	<a href="https://github.com/jeromeschneider/Baikal"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub"></a>	
 <!-- End Document
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
